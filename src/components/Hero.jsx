266c19ba4552e0caa97973ce052810e7dfb22a1a
BisiOlaYemi <ogunrinde_olayemi@yahoo.com>
2023-07-04 06:07:16 -0700

    Added my github link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -47,7 +47,7 @@ const Hero = () => {
         {/* Section two */}
         <div className='relative w-full h-full md:h-screen md:grid md:place-items-center'>
         <div className='flex justify-center mt-2 mb-2'>
-            <Link to='/'>
+            <Link to='https://github.com/BisiOlaYemi'>
               <button className='bg-gray-800 py-2 px-4 text-white text-center rounded-lg'>View details</button>
             </Link>
           </div>
