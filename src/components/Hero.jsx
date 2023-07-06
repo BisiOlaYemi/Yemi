91085b263759189f0a5554a27b126118ffa2f6d7
BisiOlaYemi <ogunrinde_olayemi@yahoo.com>
2023-07-06 04:19:55 -0700

    Added some other packages including react-icons

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,6 +9,9 @@ import simbasImg from '../assets/simbas.png';
 import charityImg from '../assets/charity.png';
 import withiresImg from '../assets/withhires.png';
 import resumePDF from '../assets/resume.pdf';
+import { useState, useEffect } from 'react';
+import { FaReact, FaPython, FaDatabase, FaNodeJs } from 'react-icons/fa';
+
 
 const Hero = () => {
   const images = [
@@ -37,6 +40,23 @@ const Hero = () => {
   const openResume = () => {
     window.open(resumePDF, '_blank');
   };
+  const skills = [
+    { id: 1, icon: <FaReact className="skill-icon" />, name: 'ReactJS' },
+    { id: 2, icon: <FaPython className="skill-icon" />, name: 'Django' },
+    { id: 3, icon: <FaDatabase className="skill-icon" />, name: 'SQL' },
+    { id: 4, icon: <FaNodeJs className="skill-icon" />, name: 'Node.js' },
+  ];
+
+  const [marqueeOffset, setMarqueeOffset] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setMarqueeOffset((prevOffset) => prevOffset - 1);
+    }, 100);
+  
+    return () => clearInterval(interval);
+  }, []);
+  
+  
 
   return (
     <BrowserRouter>
@@ -73,6 +93,19 @@ const Hero = () => {
               ))}
             </Carousel>
           </div>
+          <div className="marquee-container py-2">
+          <div
+            className="marquee flex"
+            style={{ transform: `translateX(${marqueeOffset}px)` }}
+          >
+            {skills.map((skill) => (
+              <div className="flex items-center text-rose-600 mr-8" key={skill.id}>
+                {skill.icon}
+                <span className="ml-2 text-rose-600 font-bold">{skill.name}</span>
+              </div>
+            ))}
+          </div>
+        </div>
         </div>
       </div>
     </BrowserRouter>
