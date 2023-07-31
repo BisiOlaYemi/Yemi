28578cb8d49572e893a2bf2f7bfb675ca5e329bd
BisiOlaYemi <ogunrinde_olayemi@yahoo.com>
2023-07-31 10:10:07 -0700

    Added contact to component

---
+++ src/components/Files/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div className="text-white">
+      {/* Your Contact content goes here */}
+    </div>
+  );
+};
+
+export default Contact;
