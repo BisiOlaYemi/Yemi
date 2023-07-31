29eadc6da69e0295a9d43f7e5e0a0d38ee258792
BisiOlaYemi <ogunrinde_olayemi@yahoo.com>
2023-07-31 10:08:05 -0700

    Added Resume to component

---
+++ src/components/Files/Resume.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Resume = () => {
+  return (
+    <div className="text-white">
+      {/* Your Resume content goes here */}
+    </div>
+  );
+};
+
+export default Resume;
