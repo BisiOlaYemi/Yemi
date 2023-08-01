e5ec35afccb4332329450ccb742e236427d7d2fb
BisiOlaYemi <ogunrinde_olayemi@yahoo.com>
2023-08-01 03:04:47 -0700

    Created the resume component

--- src/components/Files/Resume.jsx
+++ src/components/Files/Resume.jsx
@@ -2,8 +2,65 @@ import React from 'react';
 
 const Resume = () => {
   return (
-    <div className="text-white">
-      <h1 className='text-white'>Hello Re</h1>
+    <div className=" rounded-lg text-white p-8 relative mt-6">
+      <div className="mb-8">
+        <h1 className="text-4xl font-bold mb-2"><span className='text-indigo-600'>Yemi Ogunrinde</span></h1>
+        <p className="text-lg">Software Developer</p>
+      </div>
+      <hr className="border-t-1 border-gray-400 w-full mx-auto my-4 mb-6" />
+      <div className="mb-8">
+        <div className="thread" />
+        <h2 className="text-2xl font-bold mb-4 relative z-10"><span className='text-indigo-600'>Skills</span></h2>
+        <p>
+        JavaScript | React | Next JS | Redux | Tailwind CSS | HTML5 | CSS3 | GIT | Node| JQuery | Express | Axios | RestAPIs | MySQL | Django | NoSQL | OOP | CRUD | Backend | PHP Distributed system | AWS Cloud Computing | Frontend | Lambda | Full Stack | VPS | Nginx | Postgresql | Wordpress | MongoDB | PowerApps | Golang | SharedPoint | English – Professional
+        </p>
+      </div>
+      <hr className="border-t-1 border-gray-400 w-full mx-auto my-4 mb-6" />
+      <div className="mb-8">
+        <div className="thread" />
+        <h2 className="text-2xl font-bold mb-4 relative z-10"><span className='text-indigo-600'>Work Experience</span></h2>
+        <p className="font-bold">Software Developer, Careerhaven Consulting Inc (Remote)</p>
+        <p className="text-gray-300">Jan 2023 - June 2023</p>
+        <ul className="list-disc list-inside">
+          <li>
+          Successfully developed a sophisticated web application from scratch utilizing
+          the latest technologies.
+          </li>
+          <li>
+          Achieved a 50% increase in app stability and a 15% reduction in app crashes by
+          implementing a CI/CD Pipeline
+          </li>
+          <li>
+            Collaborated with the design team to create visually appealing and
+            user-friendly interfaces for web applications.
+          </li>
+        </ul>
+      </div>
+
+      <div className="mb-8">
+        <div className="thread" />
+        <p className="font-bold">Software Developer, Readyflow Service Limited (Hybrid)</p>
+        <p className="text-gray-300">Mar 2020 - Nov 2022</p>
+        <ul className="list-disc list-inside">
+          <li>
+            Built and successfully created a system backup for over 35,000+ customer (users) directory using SQL 
+          </li>
+          <li>
+            Developed two custom web applications using PHP, SQL, JavaScript core technologies, and Python Django.
+          </li>
+          <li>
+            Created and monitored Google Analytics for all company-associated websites, resulting in a substantial 30% upturn
+          </li>
+        </ul>
+      </div>
+      <hr className="border-t-1 border-gray-400 w-full mx-auto my-4 mb-6" />
+      <div className="mb-8">
+        <div className="thread" />
+        <h2 className="text-2xl font-bold mb-4 relative z-10"><span className='text-indigo-600'>Education</span></h2>
+        <p>
+          Bachelor of Science in Business Admin, Bangalore University, 2016
+        </p>
+      </div>
     </div>
   );
 };
