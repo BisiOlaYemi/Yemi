dde5192feb56c96ed8543cd86b58c16029cfb7ec
BisiOlaYemi <ogunrinde_olayemi@yahoo.com>
2023-08-02 00:20:46 -0700

    Updated the form

--- src/components/Files/Contact.jsx
+++ src/components/Files/Contact.jsx
@@ -61,7 +61,7 @@ const Contact = () => {
             type="submit"
             className="px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
           >
-            Send
+            Send Message
           </button>
         </form>
       </div>
