e24d10bf42cc6a9605aedb10249bd963c44a2bce
BisiOlaYemi <ogunrinde_olayemi@yahoo.com>
2023-08-03 03:26:49 -0700

    Added more project to components

--- src/components/Files/Projects.jsx
+++ src/components/Files/Projects.jsx
@@ -26,6 +26,14 @@ const Projects = () => {
       imageUrl: 'https://img.freepik.com/free-vector/placeholder-concept-illustration_114360-4727.jpg?size=626&ext=jpg&ga=GA1.1.149556823.1683788678&semt=sph',
       link: 'https://registration.careerhaven.ca/',
     },
+    {
+      imageUrl: 'https://img.freepik.com/premium-vector/mobile-calculator-service-smartphone-mobile-finance-technology-mobile-display-vector_3482-7322.jpg?size=626&ext=jpg&ga=GA1.2.149556823.1683788678&semt=ais',
+      link: 'https://innonavi.vercel.app/',
+    },
+    {
+      imageUrl: 'https://img.freepik.com/premium-vector/freelance-man-work-comfortable-cozy-home-office-kitchen-flat-illustration-freelancer-man-character-working-from-home-relaxed-pace-self-employed-concept_165217-303.jpg?size=626&ext=jpg&ga=GA1.2.149556823.1683788678&semt=sph',
+      link: 'https://enhancz.vercel.app/',
+    },
   ];
 
   return (
