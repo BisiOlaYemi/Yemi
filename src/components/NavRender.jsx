af1524065857a7ff4deab252e64fa0c816a84265
Olayemi <ogunrinde_olayemi@yahoo.com>
2023-11-05 11:04:43 -0800

    Comments

--- src/components/NavRender.jsx
+++ src/components/NavRender.jsx
@@ -4,7 +4,7 @@ function NavRender({ selectedProgram, onProgramClick }) {
   return (
     <div className="flex flex-col items-end">
       <div className="flex w-full justify-end">
-        <button className={`bg-gray-900 hover:bg-blue-400 text-white font-bold py-1 px-4 rounded mr-1 ${selectedProgram === 2 ? "bg-gray-700" : ""}`} onClick={() => onProgramClick(2)}>Resume</button>
+        {/* <button className={`bg-gray-900 hover:bg-blue-400 text-white font-bold py-1 px-4 rounded mr-1 ${selectedProgram === 2 ? "bg-gray-700" : ""}`} onClick={() => onProgramClick(2)}>Resume</button> */}
         <button className={`bg-gray-900 hover:bg-blue-400 text-white font-bold py-1 px-4 rounded mr-1 ${selectedProgram === 3 ? "bg-gray-700" : ""}`} onClick={() => onProgramClick(3)}>Projects</button>
         <button className={`bg-gray-900 hover:bg-blue-400 text-white font-bold py-1 px-4 rounded mr-1 ${selectedProgram === 4 ? "bg-gray-700" : ""}`} onClick={() => onProgramClick(4)}>Contact</button>
       </div>
