082e774b7949191f251d665a587eb662fcad9ae0
Olayemi <ogunrinde_olayemi@yahoo.com>
2024-02-27 15:25:21 +0100

    New portfolio

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,11 +13,11 @@ const Home = () => {
         <p className="text-[2.5rem] text-white text-center md:text-left lg:text-[3.5rem] font-bold tracking-wide text-headingColor md:w-[85%] md:leading-tight">
           <span className="bg-gradient-to-r from-purple-500 to-amber-500 text-transparent bg-clip-text text-[2rem] lg:text-[4rem]">Let's Build</span>  Together
         </p>
-          <p className='mt-2 text-white'>
-          Efficient transportation services for your goods and materials, latest telecommunications solutions
-          to keep your business connected. Get access to wide range of services, including voice, data,
-          and internet, We can help you plan, organize, and execute your events, from small corporate
-          meetings to large-scale festivals.
+          <p className='mt-2 text-white font-semibold'>
+          As a full-stack developer, I thrive on the dynamic interplay
+          between front-end finesse and back-end robustness, bringing visions to life.
+          With expertise spanning both ends of the development spectrum, I excel in architecting
+          and implementing end-to-end solutions that drive innovation.
           </p>
           <Link to="/contact">
             <button className="bg-blue-950 text-white md:px-7 px-4 py-2 text-lg rounded-lg hover:bg-gray-800 hover:scale-105 transition-all duration-300 mt-12">Get in touch</button>
