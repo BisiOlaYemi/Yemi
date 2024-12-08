467ac484481ad6bdd902455aca9278f1576609da
Olayemi <ogunrinde_olayemi@yahoo.com>
2024-12-08 23:19:45 +0100

    Update on chat

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ import { motion } from 'framer-motion'
 import 'tailwindcss/tailwind.css'
 import DeveloperProfile from './DeveloperProfile'
 import WorkExperienceTimeline from './WorkExperienceTimeline'
-import AiChat from './AiChat'
+// import AiChat from './AiChat'
 
 const MovingObject = ({ delay, type }) => {
   const variants = {
@@ -115,7 +115,7 @@ export default function Home() {
         </div>
         <DeveloperProfile />
         <WorkExperienceTimeline />
-        <AiChat />
+        {/* <AiChat /> */}
       </div>
     </MainLayout>
   )
