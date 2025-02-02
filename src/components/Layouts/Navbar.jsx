a66d692143a68697889540ae3d20d64b7bff2906
Olayemi <ogunrinde_olayemi@yahoo.com>
2025-02-02 20:51:50 +0100

    Added Project list

--- src/components/Layouts/Navbar.jsx
+++ src/components/Layouts/Navbar.jsx
@@ -3,12 +3,53 @@ import { HiMenuAlt3 } from 'react-icons/hi';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.png';
 
-export default function Navbar() {
+const Navbar = () => {
   const [navOpen, setNavOpen] = useState(false);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
 
   const handleNavToggle = () => {
     setNavOpen(!navOpen);
   };
+
+  const projects = [
+    { name: "FinPay Fintech", url: "https://github.com/BisiOlaYemi/finpay" },
+    { name: "Video to ppt/pdf", url: "https://github.com/BisiOlaYemi/convertvideo2pdfppt" },
+    { name: "Web Scrapper", url: "https://github.com/BisiOlaYemi/webscap" }
+  ];
+
+  const ProjectsDropdown = ({ isMobile = false }) => (
+    <div className="relative">
+      <button
+        onClick={() => setDropdownOpen(!dropdownOpen)}
+        className={`font-semibold ${
+          isMobile ? 'text-xl' : 'dancing-script-fine text-2xl'
+        } text-white hover:text-amber-500`}
+      >
+        Projects
+      </button>
+      {dropdownOpen && (
+        <div 
+          className={`absolute ${
+            isMobile ? 'relative' : 'mt-2'
+          } bg-black/95 rounded-md py-2 min-w-[160px]`}
+        >
+          {projects.map((project) => (
+            <Link
+              key={project.name}
+              to={project.url}
+              className="block px-4 py-2 bg-gray-900 text-white hover:text-amber-500 text-lg"
+              onClick={() => {
+                setDropdownOpen(false);
+                if (isMobile) setNavOpen(false);
+              }}
+            >
+              {project.name}
+            </Link>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 
   return (
     <div className="relative z-50">
@@ -31,10 +72,8 @@ export default function Navbar() {
               Contact
             </Link>
           </li>
-          <li className="font-semibold dancing-script-fine text-2xl text-white">
-            <Link to="/#" className="hover:text-amber-500">
-              Projects
-            </Link>
+          <li>
+            <ProjectsDropdown />
           </li>
         </ul>
       </div>
@@ -46,14 +85,14 @@ export default function Navbar() {
                 Contact
               </Link>
             </li>
-            <li className="font-semibold text-xl text-white">
-              <Link to="/#" className="hover:text-amber-500">
-                Projects
-              </Link>
+            <li>
+              <ProjectsDropdown isMobile={true} />
             </li>
           </ul>
         </div>
       )}
     </div>
   );
-}
+};
+
+export default Navbar;
